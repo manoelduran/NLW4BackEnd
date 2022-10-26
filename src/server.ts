01376b608ad61a495b9399ca499f67cfe667164f
Manoel Duran <manoel.duran@hotmail.com>
2022-10-26 17:15:31 -0300

    created CreateUsers migration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 
-app.listen(3333, () => console.log('RUNNING'))
+app.listen(3333, () => console.log('Server is Running on port 3333!'))
